f33a67da1248ed88389dd36440bb56a70521b9f2
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-01-20 12:02:38 +0000

    javascript functions added

---
+++ src/todo-functions.js
@@ -0,0 +1,32 @@
+const defaultProject = [];
+
+class Todo {
+    constructor(title, description, dueDate, priority, project, state){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.project=project;
+        this.state=state;
+        this.id = crypto.randomUUID();
+    }
+};
+
+const todoManager = (() => {
+    function addTodo(title, description, dueDate, priority, project, state) {
+        const newToDo = new Todo(title, description, dueDate, priority, project, state);
+        defaultProject.push(newToDo);
+
+        return newToDo
+    };
+
+    function deleteTodo(id) {
+        const toDoId = id;
+        const index = defaultProject.findIndex((item) => item.id === toDoId);
+        defaultProject.splice(index,1)
+    }
+
+    return { addTodo, deleteTodo }
+})();
+
+export {defaultProject, todoManager};
